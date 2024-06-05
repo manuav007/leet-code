c7a6b69ee94d7c796c02a451addcf7915774630f
manuav007 <131706108+manuav007@users.noreply.github.com>
2024-06-05 19:00:22 +0530

    Create 560.SubarraySumEqualsK.cpp

---
+++ 560.SubarraySumEqualsK.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int,int> mpp;
+        int count = 0;
+        int sum = 0;
+        mpp[0] = 1;
+        for(int i=0; i<nums.size(); i++){
+            sum+=nums[i];
+            int rem = sum-k;
+            count+=mpp[rem];
+            mpp[sum]++;
+        }
+        return count;
+    }
+};
