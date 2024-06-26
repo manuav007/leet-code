4e6eab1b19b279533032119723d69ac7922817a8
manuav007 <131706108+manuav007@users.noreply.github.com>
2024-06-26 23:39:07 +0530

    Create 127.WordLadder.cpp

---
+++ 127.WordLadder.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        queue<pair<string,int>> qu;
+        qu.push({beginWord,1});
+        unordered_set<string> st(wordList.begin(),wordList.end());
+        st.erase(beginWord);
+        while(!qu.empty()){
+            string word = qu.front().first;
+            int step = qu.front().second;
+            qu.pop(); 
+            if(word==endWord) return step;
+            for(int i=0; i<word.size(); i++){
+                char orig = word[i];
+                for(char ch='a'; ch<='z'; ch++){
+                    word[i] = ch;
+                    if(st.find(word) != st.end()){
+                        st.erase(word);
+                        qu.push({word,step+1});
+                    }
+                }
+                word[i] = orig;
+            }
+        }
+        return 0;
+    }
+};
