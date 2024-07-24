6db331fe6e9033a9b19fbf258f6bab0dffa7cfdb
manuav007 <131706108+manuav007@users.noreply.github.com>
2024-07-24 20:51:12 +0530

    Update 300.LongestIncreasingSubsequence.cpp

--- 300.LongestIncreasingSubsequence.cpp
+++ 300.LongestIncreasingSubsequence.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<int> ahead(n + 1, 0), curr(n + 1, 0);
-        for (int ind = n - 1; ind >= 0; ind--) {
-            for (int prevind = ind - 1; prevind >= -1; prevind--) {
-                int len = ahead[prevind + 1];
-                if (prevind == -1 || nums[ind] > nums[prevind]) {
-                    len = max(len, 1 + ahead[ind + 1]);
+        vector<int> dp(n , 1);
+        int maxi=1;
+        for(int i=0; i<n; i++){
+            for(int pre=0; pre<i; pre++){
+                if(nums[pre] < nums[i]){
+                    dp[i] = max(dp[i], 1+dp[pre]);
                 }
-                curr[prevind + 1] = len;
             }
-            ahead = curr;
+            maxi = max(maxi,dp[i]);
+            if(i==n-1) break;
         }
-        return ahead[0];
+        return maxi;
     }
 };
